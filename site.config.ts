0748021e1d3b8f2896e59b57fae5bb7c58f1dced
BJTiew <132333550+BJTiew@users.noreply.github.com>
2023-05-28 12:26:07 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '80012a4c20e94c199700f151ba6121e6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '以立留学移民',
+  domain: 'https://bjtiew.notion.site/Elite-80012a4c20e94c199700f151ba6121e6',
+  author: 'Elite',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '以立，伴您走过留学移民的道路',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
