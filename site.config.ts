0f736ce62a306402fb7175fe5a43bbbda77125b8
BJTiew <132333550+BJTiew@users.noreply.github.com>
2023-05-30 10:21:32 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -51,15 +51,15 @@ export default siteConfig({
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
   navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+   navigationStyle: 'custom',
+   navigationLinks: [
+     {
+       title: '项目',
+       pageId: '40a69e8a69414627b08a8b954357156a'
+     },
+     {
+       title: '资讯',
+       pageId: '99c28a69e3fa42a68130343e19b4a3c3'
+     }
+   ]
 })
